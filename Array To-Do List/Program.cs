108875ec6f7907e7a7cadf23b18e8d41b9be384e
JLLOYD1 <61300197+JLLOYD1@users.noreply.github.com>
2020-02-25 18:32:30 -0600

    Add files via upload

---
+++ Array To-Do List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Array_To_Do_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] Tasks = new string[7];
+
+            for (int i = 0; i < Days.Length; i++)
+            {
+                Console.WriteLine(" Type your plans for the day" + Days[i]);
+                Tasks[i] = Days[i] + ":" + Console.ReadLine();
+            }
+
+            for (int i = 0; i < Days.Length; i++) 
+            {
+                Console.WriteLine(Tasks[i]);
+            }
+        }
+    }
+}
